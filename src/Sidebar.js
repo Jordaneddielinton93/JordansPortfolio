e49d8f0406f0f14da0b8cc2c0e9a300351496b01
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2021-06-08 19:24:23 +0100

    made an email sender and fixed some bugs

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -5,12 +5,14 @@ const Sidebar = ({width,getwidth}) => {
   function changeWidth() {
     width===true ? getwidth(false): getwidth(true);
   }
-  
+  function helper(params) {
+    
+  }
   return ( 
     <aside className="aside">
       <h2 className="aside--h2" onClick={changeWidth}><i className="far fa-envelope"></i></h2>
       <h2 className="aside--h2">contact</h2>
-      <h2 className="aside--h2">help</h2>
+      <h2 className="aside--h2" onClick={helper}>helper</h2>
     </aside>
    );
 }
