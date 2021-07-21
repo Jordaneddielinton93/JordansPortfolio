f0c7a91a0b32c4a6840f377633bb27b3f27a1a71
Jordan Linton <jordaneddielinton93@hotmail.co.uk>
2021-07-21 08:44:55 +0100

    added better width to the video and added a none functioning button still need the javascript

--- src/Components/TileCards/TileCards.js
+++ src/Components/TileCards/TileCards.js
@@ -16,6 +16,7 @@ const TileCards = () => {
 
   let [showAllCards,setShowAllCards] = useState("showAllCards")
   let [cardClassSizeing,setCardClassSizeing] = useState("CardContainer-Item")
+  
   function filter(title){
     setCardClassSizeing("CardContainerBIG")
     setShowAllCards(title)
@@ -26,12 +27,8 @@ const TileCards = () => {
   
 
   class cardTiles{
-    constructor(id,title,difficulty,space,indoors,time,img,video){
+    constructor(id,title,img,video){
       this.title = title;
-      this.difficulty = difficulty;
-      this.space = space;
-      this.indoors = indoors;
-      this.time = time;
       this.img = img;
       this.video = video
       this.id=id
@@ -40,16 +37,18 @@ const TileCards = () => {
 
     makeCard(){
       return(
+  
+        
+
       <div key={this.id} 
       className={cardClassSizeing} onClick={()=>filter(this.title)} >
-        <span>
-          
-        </span>
-  
-
+        <button onClick={console.log("hellop")}>
+          X
+        </button>
+        
         <iframe className="videos" src={`https://www.youtube.com/embed/${this.video}`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 
-        <img src={this.img} alt="fruit/veg" className="mainImage"/>
+        <img src={this.img} alt="coverphoto" className="mainImage"/>
         <h2 className="CardTitle" >{this.title}</h2>
         <div className="Cardbox__centreScreen--allCards--leafContainer">
           {/* <h6 className="leafBox-h6"></h6>
@@ -67,14 +66,14 @@ const TileCards = () => {
     }
   }
 
-let Jest=new cardTiles("id1","Jest",2,3,false,3,jest,"sUJxU-bUOZs")
-let React=new cardTiles("id2","React",1,3,false,1,Blog,"Xk1qzWs65rM")
-let Codewars=new cardTiles("id3","Code wars",2,3,false,3,codewars,"SEFsIolXSYk")
-let Hologram =new cardTiles("id4","Hologram",2,3,false,3,image1,"gLqjUK0cRi0")
-let Card1=new cardTiles("id5","card1",1,2,true,2,image1,"sUJxU-bUOZs")
-let Card2=new cardTiles("id6","card2",2,2,true,2,image1,"sUJxU-bUOZs")
-let Card3=new cardTiles("id7","card3",1,2,true,2,image1,"sUJxU-bUOZs")
-let Card4=new cardTiles("id8","card4",1,2,true,3,image1,"sUJxU-bUOZs")
+let Jest=new cardTiles("id1","Jest",jest,"sUJxU-bUOZs")
+let React=new cardTiles("id2","React",Blog,"Xk1qzWs65rM")
+let Codewars=new cardTiles("id3","Code wars",codewars,"SEFsIolXSYk")
+let Hologram=new cardTiles("id4","Hologram",image1,"gLqjUK0cRi0")
+let Card1=new cardTiles("id5","card1",image1,"sUJxU-bUOZs")
+let Card2=new cardTiles("id6","card2",image1,"sUJxU-bUOZs")
+let Card3=new cardTiles("id7","card3",image1,"sUJxU-bUOZs")
+let Card4=new cardTiles("id8","card4",image1,"sUJxU-bUOZs")
 
 let UtubeCards = [
   Jest,React,Codewars,Hologram,Card1,Card2,Card3,Card4
@@ -119,10 +118,6 @@ switch (showAllCards) {
     returnedCards = Card4.makeCard()
     break
   
-
-
-
-
   default:
     break;
 }
